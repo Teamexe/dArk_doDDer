6a213f96d816ad545405ac2434fab08c7ecea83f
Nimit Bhardwaj <nimitbhardwaj@gmail.com>
2017-02-02 19:08:04 +0530

    Made the basic keystroking

---
+++ main.cpp
@@ -0,0 +1,84 @@
+//Headers for using the OS features
+#include <unistd.h>
+#include <linux/input.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+//C++ based libraries
+#include <iostream>
+#include <stdexcept>
+
+//type definations
+
+//function definations
+static void checkRoot(void);
+static const char *getKeyboardFile(void) noexcept;
+static int openKeyboardFile(void);
+
+//main
+int main()
+{
+    int kbd_Fd;
+    input_event eve;
+    checkRoot();
+    kbd_Fd = openKeyboardFile();
+    while(read(kbd_Fd, &eve, sizeof(input_event)) > 0)
+    {
+        if(eve.type == EV_KEY)
+        {
+            std::cout << eve.code << std::endl;
+        }
+        if(eve.type == EV_KEY && eve.code == 2)
+            break;
+    }
+    close(kbd_Fd);
+    return 0;
+}
+/**
+ * Function: checkRoot
+ * Arguments: none
+ * return: none
+ * use: Used to check if the user executed the program by giving superuser 
+ *      privillages
+ * Exceptions: throw runtime_error if not in root
+ **/
+
+static void checkRoot(void)
+{
+    uid_t id = geteuid(); //if used in sudo then id = 0
+    if(id != 0)
+        throw std::runtime_error("Must be in root");
+}
+
+/**
+ * Function: getKeyboardFile
+ * Arguments: none
+ * return: the path of the file which will be used to get input from keyboard
+ * use: used in function openKeyboardFile to get the path of the file for keyboard
+ * Exceptions: no exceptions
+ **/
+
+static const char *getKeyboardFile(void) noexcept
+{
+    const char *str = "/dev/input/event4";
+    return str;
+}
+
+/**
+ * Function: openKeyboardFile
+ * Arguments: none
+ * return: the file descriptor for the opened keyboard file, which will
+ *         check the keystrokes
+ * use: used to open the file for the keyboard
+ * Exceptions: throw runtime_error if file is not opened
+ **/
+
+static int openKeyboardFile(void)
+{
+    int fd = open(getKeyboardFile(), O_RDONLY);
+    if(fd < 0)
+        throw std::runtime_error("The file descriptor can't be negetive\
+                , there was error in opening the keyboard input file");
+    return fd;
+}
